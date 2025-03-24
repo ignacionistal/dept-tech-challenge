1c08d234a29fff9c00d53aeb316e8242ea4ac8b5
Ignacio Nistal <ignacionistal.96@gmail.com>
2025-03-24 20:10:00 -0300

    Node challenge task 3: Completed auth middleare to verify authorization token

--- node/src/middlewares/auth.ts
+++ node/src/middlewares/auth.ts
@@ -1,3 +1,20 @@
+import jwt from "jsonwebtoken";
+
 export const auth = (req, res, next) => {
-  return next();
+
+  const token = req.headers['authorization']?.split(" ")[1];
+
+  if(!token) {
+    return res.status(401).send("Unauthorized, missing token");
+  }
+
+  const jwtSecretKey = process.env.JWT_SECRET_KEY;
+
+  jwt.verify(token, jwtSecretKey, (error, decoded) => {
+    if(error) {
+      return res.status(401).send("Unauthorized, expired or invalid token");
+    }
+    req.currentUserId = decoded.userId;
+    return next();
+  });
 };
