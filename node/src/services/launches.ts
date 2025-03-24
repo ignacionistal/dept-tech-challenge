20a65cd51a5bbc2db9f478f29259249d7ad143a5
Ignacio Nistal <ignacionistal.96@gmail.com>
2025-03-24 20:00:53 -0300

    Node challenge task 1: Added fetch for SpaceX data and returned expected format

--- node/src/services/launches.ts
+++ node/src/services/launches.ts
@@ -3,6 +3,23 @@ import { getUserFavorites } from "./favorites";
 
 export const processLaunches = async (userId, launches, rockets) => {
   const userFavorites = await getUserFavorites(userId);
-
-  return [];
+  const launchesProccessedData = launches.map(launch => {
+    const rocketData = rockets.find(rocket => rocket.rocket_id === launch.rocket.rocket_id);
+    return {
+    flight_number: launch.flight_number,
+    mission_name: launch.mission_name,
+    mission_patch: launch.links.mission_patch,
+    details: launch.details,
+    launch_date_unix: launch.launch_date_unix,
+    rocket: {
+      rocket_id: launch.rocket.rocket_id,
+      rocket_name: rocketData.name,
+      active: rocketData.active,
+      cost_per_launch: rocketData.cost_per_launch,
+      company: rocketData.company,
+    },
+    favorite: !!userFavorites.filter(favorite => favorite.flight_number === launch.flight_number).length,
+    }
+  });
+  return launchesProccessedData;
 };
